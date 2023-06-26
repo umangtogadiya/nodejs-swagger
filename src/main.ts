2a74c90f11b1a9b1b78f5fa298d4fb8bea63c13b
unknown <togadiyaumang@gmail.com>
2023-06-26 13:08:35 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/", routes);
-app.get("/tets", (req: Request, res: Response) => {
-  res.send({ status: "success" });
-});
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log("❌ ", err);
   return res.status(500).send("Something went wrong!");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3111;
 app.listen(port, () => {
   console.log(`✅ Server Listening on ${port}`);
   swaggerDocs(app);
